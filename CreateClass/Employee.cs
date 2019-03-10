40c4321f4ad90edbf14bc4475fc3a85202fdd226
Molnár Attila <attila.molnar@codecool.com>
2019-03-10 14:05:44 +0100

    Answer your questions regarding Employee class.
    Please check

--- CreateClass/Employee.cs
+++ CreateClass/Employee.cs
@@ -8,12 +8,28 @@ namespace CreateClass
 {
     public class Employee : Person, ICloneable
     {
+        /* Don't use private properties because this way it becomes purposeless.
+        modify like this. This is named full-property which means you implement the backing private field as well:
+        private int _salary;
+        
+        public int Salary
+        {
+            get { return _salary; }
+            set { _salary = value; }
+        }
+        
+        if it's enough the very basic getting and setting mechanism than you can use so-called auto-property:
+        public int AutoSalary { get; set; }
+        It generates the same code as above but in the background.
+        
+        check the following link: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/properties
+        */
         private int Salary { get; set; }
         private string Profession { get; set; }
         protected Room RoomNumber { get; set; }
 
         // Why do I have to add roomNumber in the constructor? How could I add a roomNumber after creating the employee with its setter?
-
+        // You don't need it in the constructor. Actually, right now itscontrsuctor getting too complex because of the too many params
         public Employee(string name, int birthDate, Gender gender, int salary, string profession, Room roomNumber) :
             base(name, birthDate, gender)
         {
@@ -25,7 +41,8 @@ namespace CreateClass
         /* WHY won't this work??? 
         public setRoomNumber(int roomNumber)
         {
-            this.RoomNumber = roomNumber;
+            this.RoomNumber = roomNumber; // Because the Room is not an Integer value. You have to instantiate the Room with the value coming from the parameter
+            // this.RoomNumber = new Room(roomNumber);
         }
         */
 
@@ -36,7 +53,17 @@ namespace CreateClass
         }
 
         // If I write "override" instead of "new" at ToString(), then it will ovverride its base class no matter what, and I will not be able
-        // to print the Person properties for an employee object (only the employee properties)! 
+        // to print the Person properties for an employee object (only the employee properties)!
+        
+        /* You can refer to your base class' fields and methods (including ToString()) with the `base` keyword
+        public override string ToString()
+        {
+            return $"{base.ToString()}, salary: {Salary}, profession: {Profession}, room: {Room.Number}";
+        }
+        
+        If you use the `new` keyword it will totally hide the base class implementation. You don't need that in this situation
+        Check: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/classes-and-structs/versioning-with-the-override-and-new-keywords
+        */
     
 
     public object Clone()
